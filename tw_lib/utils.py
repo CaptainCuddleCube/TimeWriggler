b69a31b5c2c5422b9277b7313de01cd9ef876b16
Ashton Hudson <ashtonhudson1@gmail.com>
2020-05-05 11:47:47 +0200

    fix: Fixed the aggregation bug.

--- tw_lib/utils.py
+++ tw_lib/utils.py
@@ -7,11 +7,17 @@ def group_by_date(data, date_format, round_up=False):
     group_by_date = {}
     for entry in data:
         date = datetime.fromisoformat(entry["start"]).date().strftime(date_format)
-        if date in group_by_date:
-            group_by_date[date][-1] += entry["duration"] / 60 / 60
-        else:
+        project, task = entry["name"].split(" | ")
+        if f"{date}|{project}|{task}" in group_by_date and entry["duration"] > 0:
+            group_by_date[f"{date}|{project}|{task}"][-1] += entry["duration"] / 60 / 60
+        elif entry["duration"] > 0:
             project, task = entry["name"].split(" | ")
-            group_by_date[date] = [date, project, task, entry["duration"] / 60 / 60]
+            group_by_date[f"{date}|{project}|{task}"] = [
+                date,
+                project,
+                task,
+                entry["duration"] / 60 / 60,
+            ]
     if round_up:
         for v in group_by_date.values():
             if isclose(v[-1], int(v[-1]), abs_tol=0.1):
